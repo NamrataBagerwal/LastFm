b91e25591bc6a6292ed2aee2a072730e64e5cc90
Namrata Bagerwal <namratabagerwal@gmail.com>
2020-09-02 23:50:02 +0100

    Test Commit

--- app/src/main/java/com/search/lastfm/LastFmApplication.kt
+++ app/src/main/java/com/search/lastfm/LastFmApplication.kt
@@ -5,7 +5,7 @@ import com.search.lastfm.di.DependencyInjectionModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
-class LastFmApplication: Application() {
+class LastFmApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
